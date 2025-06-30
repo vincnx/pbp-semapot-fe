5e8daaa69b9f22594cd5cf9b1498765e6e8c16e1
vincent exelcio <vincent.celcio@gmail.com>
2025-06-30 07:39:40 +0700

    chore: change class table to use dummy data

--- src/routes/__sidebarLayout/principal/classes/index.tsx
+++ src/routes/__sidebarLayout/principal/classes/index.tsx
@@ -1,14 +1,37 @@
 import { ClassDataTable } from "@/features/principal/classes/components/class-datatable";
 import { columns } from "@/features/principal/classes/components/columns";
-import { useFetchClasses } from "@/features/principal/hooks/useFetchClasses";
+import type { Class } from "@/types/class.type";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/__sidebarLayout/principal/classes/")({
   component: RouteComponent,
+  loader: async () => {
+    return {
+      data: await getData(),
+    };
+  },
 });
 
+async function getData(): Promise<Class[]> {
+  return [
+    {
+      id: 1,
+      grade: 1,
+      code: "a",
+      user: {
+        id: 1,
+        email: "teacher@test.com",
+        name: "Teacher",
+        role: "teacher",
+      },
+      year: 2025,
+    },
+  ];
+}
+
 function RouteComponent() {
-  const { data } = useFetchClasses();
+  // const { data } = useFetchClasses();
+  const { data } = Route.useLoaderData();
 
   return (
     <>
